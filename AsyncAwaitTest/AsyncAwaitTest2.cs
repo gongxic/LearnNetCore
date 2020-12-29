78c7b4f0eda1c908adaa3ad72dfbaa5fa6fe4225
Xicheng.Gong <gongxicheng1111@hotmail.com>
2020-12-29 16:23:15 +0800

    add test

---
+++ AsyncAwaitTest/AsyncAwaitTest2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTest
+{
+    public class AsyncAwaitTest2
+    {
+        private static readonly Stopwatch Watch = new Stopwatch();
+
+        public void Test()
+        {
+            Watch.Start();
+
+            const string url1 = "http://www.cnblogs.com/";
+            const string url2 = "http://www.cnblogs.com/liqingwen/";
+
+            //两次调用 CountCharacters 方法（下载某网站内容，并统计字符的个数）
+            var result1 = CountCharacters(1, url1);
+            var result2 = CountCharacters(2, url2);
+
+            //三次调用 ExtraOperation 方法（主要是通过拼接字符串达到耗时操作）
+            for (var i = 0; i < 3; i++)
+            {
+                ExtraOperation(i + 1);
+            }
+
+            //控制台输出
+            Console.WriteLine($"{url1} 的字符个数：{result1.Result}");
+            Console.WriteLine($"{url2} 的字符个数：{result2.Result}");
+
+        }
+
+        private async Task<int> CountCharacters(int id, string address)
+        {
+            var wc = new WebClient();
+            Console.WriteLine($" 开 始调用 id = {id}：{Watch.ElapsedMilliseconds} ms");
+
+            var result = await wc.DownloadStringTaskAsync(address);
+            Console.WriteLine($" 调 用完成 id = {id}：{Watch.ElapsedMilliseconds} ms");
+
+            return result.Length;
+        }
+
+
+        private static void ExtraOperation(int id)
+        {
+            var s = "";
+
+            for (var i = 0; i < 6000; i++)
+            {
+                s += i;
+            }
+
+            Console.WriteLine($"id = {id} 的 ExtraOperation 方法完成：{Watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
